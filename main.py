cb41ac67050bdd22a15e37ea16935dcd8b0dd1ba
JackTroy <691729768@qq.com>
2018-06-11 16:26:28 +0800

    update main

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from feature_extractor import get_features
 from xg_model import xgb_score
 
 if __name__ == '__main__':
-    features, feature_type, flg = get_features(regenerate=True)
+    train_features, test_features, train_flg, test_flg, feature_types = get_features(regenerate=True)
 
