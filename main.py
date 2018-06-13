d4f12ef6bb4a4c4a7fbaeba4f97b9c8709f2a832
Ldwayo <lindongwangyu@qq.com>
2018-06-13 16:43:13 +0800

    update

--- main.py
+++ main.py
@@ -5,17 +5,15 @@ from xg_model import save_result
 
 if __name__ == '__main__':
     train, test, feature_types = get_features(regenerate=False)
+    # get model score
+    # the default cv is 5
+    xgb_score(train)
 
-# get model score
-# the default cv is 5
-xgb_score(train)
+    # get model predict
+    predict = xgb_model(train[0], train[1], test[0])
 
-# get model predict
-predict = xgb_model(train[0], train[1], test[0])
-
-# save model predict to prediction/
-# return is the csv content. type is Dataframe
-# the default csv name is test_result
-result = save_result(train, test)
-print(result.head())
-
+    # save model predict to prediction/
+    # return is the csv content. type is Dataframe
+    # the default csv name is test_result
+    result = save_result(train, test)
+    print(result.head())
